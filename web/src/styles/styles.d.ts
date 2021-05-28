d9cf90236697f6916084d52af03a85ffa7f17fba
Jonathan Ferraz <jonathan.chagas@e-unicred.com.br>
2021-05-28 00:05:54 -0300

    refactor(tipagem): ajustando tipagem das cores

--- web/src/styles/styles.d.ts
+++ web/src/styles/styles.d.ts
@@ -2,32 +2,18 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    colorBackground: string | any;
-    colorBackground2: string | any;
-    colorGreen: string | any;
-    colorGreenLow: string | any;
-    colorRed: string | any;
-    colorOrange: string | any;
-    colorYellow: string | any;
-    colorAttention: string | any;
-    colorPrimary: string | any;
-    colorPrimaryHover: string | any;
-    colorPrimaryBorder: string | any;
-    colorPrimaryShape: string | any;
-    colorSecondary: string | any;
-    colorText: string | any;
-    colorSupport: string | any;
-    colorShape: string | any;
-    colorShapeHover: string | any;
-    colorIcons: string | any;
-    colorAttention2: string | any;
-    colorBorders: string | any;
-    colorExperts: string | any;
-    colorBlack: string | any;
-    colorWhite: string | any;
-    colorScrollbar: string | any;
-    colorScrollbarBackground: string | any;
-    colorGradient: string | any;
-    text: string | any;
+    colorBackground: string;
+    colorBackground2: string;
+    colorShape: string;
+    colorBorders: string;
+    colorRed: string;
+    colorYellow: string;
+    colorGreen: string;
+    colorPrimary: string;
+    colorSecondary: string;
+    colorText: string;
+    colorSupport: string;
+    colorWhite: string;
+    colorScrollbarBackground: string;
   }
 }
--- web/src/styles/themes/defaultTheme.ts
+++ web/src/styles/themes/defaultTheme.ts
@@ -1,30 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  colorGreenLow: '#1c2a24',
-
-  colorOrange: '#ff7a29',
-
-  colorAttention: '#332616',
-
-  colorPrimaryHover: '#9466ff',
-  colorPrimaryBorder: '#6833e4',
-  colorPrimaryShape: '#734bd1',
-
-  colorShapeHover: '#29292e',
-  colorIcons: '#41414d',
-  colorAttention2: '#fd951f',
-
-  colorExperts: '#d6ac4e',
-  colorBlack: '#09090a',
-
-  colorScrollbar: '#4d4d57',
-  colorScrollbarBackground: 'transparent',
-  colorGradient:
-    'linear-gradient(89.11deg, rgba(113, 104, 212, 0.1607843137254902) 1.53%, rgba(43, 200, 227, 0.1607843137254902) 98.35%)',
-  text: '18px',
-
-  // ====================================
-
   colorBackground: '#121214',
   colorBackground2: 'rgb(32, 32, 36)',
   colorShape: '#202024',
@@ -37,4 +12,6 @@ export default {
   colorText: '#a8a8b3',
   colorSupport: '#737380',
   colorWhite: '#fff',
+  colorScrollbar: '#4d4d57',
+  colorScrollbarBackground: 'transparent',
 };
